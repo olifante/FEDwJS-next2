470cc746a768d1c6f9d43402272213ff83b99fbe
Tiago Henriques <trinosauro@gmail.com>
2017-06-19 23:02:52 +0200

    Add Head

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,10 @@
+import Head from 'next/head'
 export default () => (
-  <div>Welcome to next.js!</div>
+  <div>
+    <Head>
+      <title>My page title</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    <p>Hello world!</p>
+  </div>
 )
