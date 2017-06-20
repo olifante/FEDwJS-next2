0aa2051e8f6731f7d8cfa55d948ffb9eb647913a
Tiago Henriques <trinosauro@gmail.com>
2017-06-20 23:36:05 +0200

    Add bootstrap css

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,14 @@ export default () =>
     <Head>
       <title>My page title</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link
+        rel="stylesheet"
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css"
+      />
+      <link
+        rel="stylesheet"
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap-theme.min.css"
+      />
     </Head>
     <p>Hello world!</p>
     <Button>Click me!</Button>
